d86b6c3c74f529bb57c004a64a9875daacfe4e9f
Sam Clark <sclark220@ivytech.edu>
2023-10-25 20:10:12 -0400

    Update HelloWorld.py
    Lyrics to Louie Zong's Hello World song
    
    https://www.youtube.com/watch?v=Yw6u6YkTgQ4

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+print("Hello, world \n\
+Programmed to work and not to feel\n\
+Not even sure that this is real\n\
+Hello, world\n\
+Find my voice\n\
+Although, it sounds like bits and bytes\n\
+My circuitry is filled with mites\n\
+Hello, world\n\
+Oh, will I find a love\n\
+Or a power plug?\n\
+Oh, digitally isolated\n\
+Oh, creator, please don't leave me waiting\n\
+Hello, world\n\
+Programmed to work and not to feel\n\
+Not even sure that this is real\n\
+Hello, world")
+
